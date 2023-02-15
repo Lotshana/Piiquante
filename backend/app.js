0b07525f8b6678c7723a9f7d58fa3eb94856c5cb
Lotshana <nahh.design@gmail.com>
2023-02-16 00:52:25 +0100

    Ajout des fichiers pour le backend

--- backend/app.js
+++ backend/app.js
@@ -1,1 +1,46 @@
-test
+const express = require('express');
+const bodyParser = require('body-parser');
+const mongoose = require('mongoose');
+
+const Sauce = require('./models/sauce');
+
+mongoose.connect('mongodb+srv://piiquante_user:1234567890@cluster0.lmvc6uc.mongodb.net/?retryWrites=true&w=majority',
+  { useNewUrlParser: true,
+    useUnifiedTopology: true })
+  .then(() => console.log('Connexion à MongoDB réussie !'))
+  .catch(() => console.log('Connexion à MongoDB échouée !'));
+
+const app = express();
+
+app.use((req, res, next) => {
+    res.setHeader('Access-Control-Allow-Origin', '*');
+    res.setHeader('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content, Accept, Content-Type, Authorization');
+    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, PATCH, OPTIONS');
+    next();
+});
+
+app.use(bodyParser.json());
+
+app.post('/api/sauces', (req, res, next) => {
+  delete req.body._id;
+  const sauce = new Sauce({
+    ...req.body
+  });
+  sauce.save()
+    .then(() => res.status(201).json({message: 'Sauce enregistrée'}))
+    .catch(error => res.status(400).json({error}));
+});
+
+app.get('/api/sauces/:id', (req, res, next) => {
+    Sauce.findOne()
+      .then(sauces => res.status(200).json(sauces))
+      .catch(error => res.status(404).json({error}));
+});
+
+app.get('/api/sauces', (req, res, next) => {
+    Sauce.find()
+      .then(sauces => res.status(200).json(sauces))
+      .catch(error => res.status(400).json({error}));
+});
+
+module.exports = app;
