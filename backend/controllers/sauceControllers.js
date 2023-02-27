76f56bcaba49d321971816e0ddfcc2f4cdee7f4a
Lotshana <nahh.design@gmail.com>
2023-02-27 16:49:54 +0100

    Correction d'une erreur de frappe

--- backend/controllers/sauceControllers.js
+++ backend/controllers/sauceControllers.js
@@ -42,7 +42,7 @@ exports.deleteSauce = (req, res, next) => {
         if (sauce.userId != req.auth.userId) {
           res.status(401).json({message: 'Not authorized'})
         } else {
-          const filename = sauce.imageUrl.splut('/images/')[1];
+          const filename = sauce.imageUrl.split('/images/')[1];
           fs.unlink(`images/${filename}`, () => {
             Sauce.deleteOne({_id: req.params.id})
               .then(() => {res.status(200).json({message: 'Sauce supprimée )'})})
